42e24687b5bdea64c10d08685cb8090a7780238a
dev soft <dev.soft687@gmail.com>
2025-03-05 10:15:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+
+// const PORT =5000;
+const app=express()
+app.use(cors({ origin: "*" }));
+app.use(express);
+
+
+
+app.post('/api/store_data', async (req, res) => {
+    
+
+    try {
+        return res.status(200).json(req.body);
+        
+    } catch (error) {
+        console.error('Upload error:', error);
+        res.status(500).json({ error: 'Failed to upload video' });
+    }
+});
+
+
+/** test URL */
+app.get('/',(req,res)=>{
+    res.end("<h1>Base Directory</h1>")
+})
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+//   });
+
+module.exports = app;
